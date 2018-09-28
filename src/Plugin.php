510f0deea0e7e8abf5b7279f80eff211e9b2edf9
Joe Huss <detain@interserver.net>
2018-09-27 23:42:37 -0400

    updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -54,10 +54,10 @@ class Plugin
 	public static function getRequirements(GenericEvent $event)
 	{
 		$loader = $event->getSubject();
-		$loader->add_requirement('class.Kayako', '/../vendor/detain/kayako-chat/src/Kayako.php');
-		$loader->add_requirement('deactivate_kcare', '/../vendor/detain/kayako-chat/src/abuse.inc.php');
-		$loader->add_requirement('deactivate_abuse', '/../vendor/detain/kayako-chat/src/abuse.inc.php');
-		$loader->add_requirement('get_abuse_licenses', '/../vendor/detain/kayako-chat/src/abuse.inc.php');
+		$loader->add_requirement('class.Kayako', '/../vendor/detain/myadmin-kayako-chat/src/Kayako.php');
+		$loader->add_requirement('deactivate_kcare', '/../vendor/detain/myadmin-kayako-chat/src/abuse.inc.php');
+		$loader->add_requirement('deactivate_abuse', '/../vendor/detain/myadmin-kayako-chat/src/abuse.inc.php');
+		$loader->add_requirement('get_abuse_licenses', '/../vendor/detain/myadmin-kayako-chat/src/abuse.inc.php');
 	}
 
 	/**
